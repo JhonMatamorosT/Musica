d87699290299d38d901229a0ea6bdd47ebb4f9b7
matam <matamorosjhon1@gmail.com>
2024-04-30 00:45:20 -0500

    SUBIDA CASA

--- app/src/main/java/com/undiremos/musica/MainActivity.kt
+++ app/src/main/java/com/undiremos/musica/MainActivity.kt
@@ -5,13 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.zjhon10.musica.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        print("demo prueba 21")
+        print("demo prueba 21 CAS")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

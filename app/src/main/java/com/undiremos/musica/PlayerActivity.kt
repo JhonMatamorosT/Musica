521773e63b79955c2820c9e2f59f9da21e6947d4
Jhon MT <undiremos@gmail.com>
2024-05-03 17:46:01 -0500

    avance de inicio

--- app/src/main/java/com/undiremos/musica/PlayerActivity.kt
+++ app/src/main/java/com/undiremos/musica/PlayerActivity.kt
@@ -6,13 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setTheme(R.style.Theme_Musica)
-        setContentView(R.layout.activity_main)
-        print("demo prueba 21 CAS")
+        setContentView(R.layout.activity_player)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

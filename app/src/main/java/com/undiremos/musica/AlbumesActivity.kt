bbb3ec32cd4f34e6f6f7f43569c675b391e4c56c
Jhon MT <undiremos@gmail.com>
2024-05-14 14:35:14 -0500

    no puedo con el binding

--- app/src/main/java/com/undiremos/musica/AlbumesActivity.kt
+++ app/src/main/java/com/undiremos/musica/AlbumesActivity.kt
@@ -1,5 +1,6 @@
 package com.undiremos.musica
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,10 +8,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class AlbumesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAlbumesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setTheme(R.style.Theme_Musica)
         setContentView(R.layout.activity_albumes)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
